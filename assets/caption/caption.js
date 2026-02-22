9f51d54a37ca03572e56da2666d75534f7b1681f
MinhAn1808 <ankhangloc12345@gmail.com>
2026-02-22 18:21:51 +0700

    Add files via upload

---
+++ assets/caption/caption.js
@@ -0,0 +1,25 @@
+let captions = [
+
+"Khoảnh khắc đi chơi xa đầu tiên của tụi mình ❤️",
+
+"Em là điều đẹp nhất đối với anh",
+
+"Hy vọng tụi mình sẽ còn nhiều năm nữa bên nhauu",
+
+"Anh yêu em ❤️",
+
+"Anh sẽ luôn ở đây, bên cạnh em",
+
+"Chỉ cần em cười, mọi thứ với anh đều đáng giá",
+
+"Ngày bình thường cũng trở nên đặc biệt khi có ẻmm",
+
+"Sau này mình vẫn nắm tay nhau như hôm nay nhaaa",
+
+"Thêm một kỷ niệm đẹp của tụi mìnhh",
+
+"Cảm ơn em đã đến trong cuộc đời anh",
+
+"Anh muốn bên cạnh em thật lâu, hết thời gian còn lại của anh"
+
+]
